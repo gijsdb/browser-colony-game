f0c88b95bd389421fd6fe826abb6f661fdc7061d
gijsdb <gijsdeblauw@gmail.com>
2024-07-27 17:05:18 +1200

    refactor: created class TerrainGenerator to be used by MapScene

---
+++ client/src/game/TerrainGenerator.js
@@ -0,0 +1,91 @@
+import { createNoise2D } from 'simplex-noise';
+
+const TILE_VARIANTS = {
+    grass: { id: 16 },
+    dirt: { id: 156 },
+    mountain: { id: 291 },
+    water: { id: 106 },
+    water_edge_top: { id: 46 },
+    water_edge_bottom: { id: 76 },
+    water_edge_left: { id: 60 },
+    water_edge_right: { id: 62 },
+    water_corner_topleft: { id: 45 },
+    water_corner_topright: { id: 47 },
+    water_corner_bottomleft: { id: 75 },
+    water_corner_bottomright: { id: 77 }
+};
+
+
+export default class TerrainGenerator {
+    constructor() {
+
+    }
+
+
+    generateTerrainPerlinNoise(width, height) {
+        const noise = createNoise2D();
+        const terrain = [];
+        for (let y = 0; y < height; y++) {
+            terrain[y] = [];
+            for (let x = 0; x < width; x++) {
+                const value = noise(x / 20, y / 20);
+                if (value < -0.7) {
+                    terrain[y][x] = TILE_VARIANTS.water.id;
+                } else if (value < 0) {
+                    terrain[y][x] = TILE_VARIANTS.grass.id;
+                } else if (value < 0.5) {
+                    terrain[y][x] = TILE_VARIANTS.dirt.id;
+                } else {
+                    terrain[y][x] = TILE_VARIANTS.mountain.id;
+                }
+            }
+        }
+
+        this.applyWaterEdges(terrain);
+
+        return terrain;
+    }
+
+    applyWaterEdges(terrain) {
+        for (let y = 0; y < terrain.length; y++) {
+            for (let x = 0; x < terrain[y].length; x++) {
+                if (terrain[y][x] === TILE_VARIANTS.water.id) {
+                    terrain[y][x] = this.getWaterEdgeTile(terrain, x, y);
+                }
+            }
+        }
+    }
+
+    getTile(terrain, x, y) {
+        if (x < 0 || y < 0 || x >= terrain[0].length || y >= terrain.length) {
+            return null;
+        }
+        return terrain[y][x];
+    }
+
+    getWaterEdgeTile(terrain, x, y) {
+        const isWater = (x, y) => this.getTile(terrain, x, y) === TILE_VARIANTS.water.id;
+
+        const top = isWater(x, y - 1);
+        const bottom = isWater(x, y + 1);
+        const left = isWater(x - 1, y);
+        const right = isWater(x + 1, y);
+
+        const topLeft = isWater(x - 1, y - 1);
+        const topRight = isWater(x + 1, y - 1);
+        const bottomLeft = isWater(x - 1, y + 1);
+        const bottomRight = isWater(x + 1, y + 1);
+
+        if (top && left && !topLeft) return TILE_VARIANTS.water_corner_topleft.id;
+        if (top && right && !topRight) return TILE_VARIANTS.water_corner_topright.id;
+        if (bottom && left && !bottomLeft) return TILE_VARIANTS.water_corner_bottomleft.id;
+        if (bottom && right && !bottomRight) return TILE_VARIANTS.water_corner_bottomright.id;
+
+        if (top && !left && !right) return TILE_VARIANTS.water_edge_top.id;
+        if (bottom && !left && !right) return TILE_VARIANTS.water_edge_bottom.id;
+        if (left && !top && !bottom) return TILE_VARIANTS.water_edge_left.id;
+        if (right && !top && !bottom) return TILE_VARIANTS.water_edge_right.id;
+
+        return TILE_VARIANTS.water.id;
+    }
+}
--- client/src/game/scenes/MapScene.js
+++ client/src/game/scenes/MapScene.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import { createNoise2D } from 'simplex-noise';
 import tiles from "../../assets/tilesets/1.png";
+import TerrainGenerator from '../TerrainGenerator';
 
 const TILE_VARIANTS = {
     grass: { id: 16 },
@@ -21,6 +22,7 @@ class MapScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
 
+        this.terrain_gen = new TerrainGenerator()
         this.targetZoom = 1;
         this.zoomSpeed = 0.1;
     }
@@ -44,7 +46,7 @@ class MapScene extends Phaser.Scene {
         const tileset = map.addTilesetImage('tileset-name', 'tiles');
         const layer = map.createBlankLayer('Ground', tileset);
 
-        const terrain = this.generateTerrain(mapWidth, mapHeight);
+        const terrain = this.terrain_gen.generateTerrainPerlinNoise(mapWidth, mapHeight)
 
         for (let y = 0; y < mapHeight; y++) {
             for (let x = 0; x < mapWidth; x++) {
@@ -54,6 +56,27 @@ class MapScene extends Phaser.Scene {
         }
 
         this.cameras.main.setBounds(0, 0, mapWidth * tileSize, mapHeight * tileSize);
+
+        this.setInputHandlers()
+    }
+
+    update() {
+        const cam = this.cameras.main;
+        cam.zoom += (this.targetZoom - cam.zoom) * this.zoomSpeed;
+        if (this.wasd.W.isDown) {
+            cam.scrollY -= 10;
+        } else if (this.wasd.S.isDown) {
+            cam.scrollY += 10;
+        }
+
+        if (this.wasd.A.isDown) {
+            cam.scrollX -= 10;
+        } else if (this.wasd.D.isDown) {
+            cam.scrollX += 10;
+        }
+    }
+
+    setInputHandlers() {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.wasd = this.input.keyboard.addKeys('W,S,A,D');
 
@@ -103,91 +126,6 @@ class MapScene extends Phaser.Scene {
             const zoomChange = deltaY * 0.001; // Adjust this value for zoom sensitivity
             this.targetZoom = Phaser.Math.Clamp(this.cameras.main.zoom - zoomChange, 0.5, 3);
         });
-    }
-
-    update() {
-
-        const cam = this.cameras.main;
-        cam.zoom += (this.targetZoom - cam.zoom) * this.zoomSpeed;
-        if (this.wasd.W.isDown) {
-            cam.scrollY -= 10;
-        } else if (this.wasd.S.isDown) {
-            cam.scrollY += 10;
-        }
-
-        if (this.wasd.A.isDown) {
-            cam.scrollX -= 10;
-        } else if (this.wasd.D.isDown) {
-            cam.scrollX += 10;
-        }
-    }
-
-    generateTerrain(width, height) {
-        const noise = createNoise2D();
-        const terrain = [];
-        for (let y = 0; y < height; y++) {
-            terrain[y] = [];
-            for (let x = 0; x < width; x++) {
-                const value = noise(x / 20, y / 20);
-                if (value < -0.7) {
-                    terrain[y][x] = TILE_VARIANTS.water.id;
-                } else if (value < 0) {
-                    terrain[y][x] = TILE_VARIANTS.grass.id;
-                } else if (value < 0.5) {
-                    terrain[y][x] = TILE_VARIANTS.dirt.id;
-                } else {
-                    terrain[y][x] = TILE_VARIANTS.mountain.id;
-                }
-            }
-        }
-
-        this.applyWaterEdges(terrain);
-
-        return terrain;
-    }
-
-    applyWaterEdges(terrain) {
-        for (let y = 0; y < terrain.length; y++) {
-            for (let x = 0; x < terrain[y].length; x++) {
-                if (terrain[y][x] === TILE_VARIANTS.water.id) {
-                    terrain[y][x] = this.getWaterEdgeTile(terrain, x, y);
-                }
-            }
-        }
-    }
-
-    getTile(terrain, x, y) {
-        if (x < 0 || y < 0 || x >= terrain[0].length || y >= terrain.length) {
-            return null;
-        }
-        return terrain[y][x];
-    }
-
-    // WIP
-    getWaterEdgeTile(terrain, x, y) {
-        const isWater = (x, y) => this.getTile(terrain, x, y) === TILE_VARIANTS.water.id;
-
-        const top = isWater(x, y - 1);
-        const bottom = isWater(x, y + 1);
-        const left = isWater(x - 1, y);
-        const right = isWater(x + 1, y);
-
-        const topLeft = isWater(x - 1, y - 1);
-        const topRight = isWater(x + 1, y - 1);
-        const bottomLeft = isWater(x - 1, y + 1);
-        const bottomRight = isWater(x + 1, y + 1);
-
-        if (top && left && !topLeft) return TILE_VARIANTS.water_corner_topleft.id;
-        if (top && right && !topRight) return TILE_VARIANTS.water_corner_topright.id;
-        if (bottom && left && !bottomLeft) return TILE_VARIANTS.water_corner_bottomleft.id;
-        if (bottom && right && !bottomRight) return TILE_VARIANTS.water_corner_bottomright.id;
-
-        if (top && !left && !right) return TILE_VARIANTS.water_edge_top.id;
-        if (bottom && !left && !right) return TILE_VARIANTS.water_edge_bottom.id;
-        if (left && !top && !bottom) return TILE_VARIANTS.water_edge_left.id;
-        if (right && !top && !bottom) return TILE_VARIANTS.water_edge_right.id;
-
-        return TILE_VARIANTS.water.id;
     }
 }
 
